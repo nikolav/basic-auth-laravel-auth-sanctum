a9562f2a796093986e232377702ba8e4f3c3a4e8
nikolav <admin@nikolav.rs>
2022-08-12 17:55:00 +0100

    heroku pg db

---
+++ app/init.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+# php artisan clear
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan view:clear
+# php artisan route:clear
+
+# composer install --no-interaction
+# # yarn install
